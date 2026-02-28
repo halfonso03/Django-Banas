c0a9f4f1a7815ca515d49b7a43a0435ce8682479
halfonso <hector.alfonso@yahoo.com>
2026-02-28 17:16:06 -0500

    Added Auth

---
+++ bobslist/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    street = models.CharField(max_length=40)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=20)
+    zip_code = models.PositiveIntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    
+    
