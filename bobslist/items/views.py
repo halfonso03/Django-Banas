a42d0ef80d87ee3ca358663cc811e9c2c03038d7
halfonso <hector.alfonso@yahoo.com>
2026-03-03 11:13:58 -0500

    Item Detail update

--- bobslist/items/views.py
+++ bobslist/items/views.py
@@ -2,30 +2,51 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Item
+from django.contrib.auth.mixins import (LoginRequiredMixin, UserPassesTestMixin)
 
-class ItemListView(ListView):
+class ItemListView(LoginRequiredMixin, ListView):
     model = Item
     template_name = "item_list.html"
+    login_url = "login"
         
-class ItemDetailView(DetailView):
+class ItemDetailView(LoginRequiredMixin, DetailView):
     model = Item
     template_name = "item_detail.html"
+    login_url = "login"
     
-class ItemUpdateView(UpdateView):
+    
+class ItemUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Item
     fields = ('title', 'description', 'price')
     template_name = "item_edit.html"
+    login_url = "login"
     
-class ItemDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.seller == self.request.user
+    
+    
+class ItemDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Item
     template_name = "item_delete.html"
     success_url = reverse_lazy('item_list')
+    login_url = "login"
     
-class ItemCreateView(CreateView):
+    def test_func(self, form):
+        obj = self.get_object()
+        return obj.seller == self.request.user
+    
+    
+class ItemCreateView(LoginRequiredMixin, CreateView):
     model = Item
     template_name = "item_new.html"
     fields = ('title', 'description', 'seller', 'price', 'photos')
     success_url = reverse_lazy('item_create')
+    login_url = "login"
+    
+    def form_valid(self, form):
+        form.instance.seller = self.request.seller
+        return super().form_valid(form)
     
 
     
